a75d497ae1e36c1a3b7b878de22ed392ff184717
Nanotixx <166376014+Nanotixx@users.noreply.github.com>
2024-06-23 14:38:24 +0800

    Add files via upload

---
+++ TurnBasedLastAlphaTest.py
@@ -0,0 +1,92 @@
+#Made By Nanotixx/Hanprince
+import random
+import colorama
+from colorama import init, Fore, Back, Style
+import time
+
+Boss = 100
+Player = 100
+print("Welcome To TurnBased Game By Nanotixx! | Version: 1.1 Alpha")
+
+while True:
+    print("\n[HP: " + Fore.GREEN + str(Player) + Fore.WHITE + "]" + "   [Boss HP: " + Fore.RED + str(Boss) + Fore.WHITE + "]")
+    
+    while True:
+        In = input("Attack - 1 Heal - 2: ")
+        if In == "1" or In == "2":
+            break
+        else:
+            print("Invalid Command.")
+    
+    if In == "1":
+        Dice = random.randint(1, 30)
+        Dice2 = random.randint(1, 10)
+        Dice3 = random.randint(1, 30)
+        SecretDice = random.randint(1, 1000)
+        if SecretDice == 24:
+            Boss = Boss - 1000
+            print(Fore.RED + "\nBLACK FLASH! 1 out of 1000 Miracle! The Boss HP Is Now " + str(Boss) + Fore.WHITE + "\n")
+            print("You Won!")
+            exit()
+        Boss = Boss - Dice
+        print("\nYou Damaged The Boss By " + Fore.BLUE + str(Dice) + Fore.WHITE + " The Boss Health Is Now " + Fore.RED +str(Boss) + Fore.WHITE +"\n")
+        
+        if Dice > 20:
+            print(Fore.GREEN + "A Critical Hit!" + Fore.WHITE
+            )
+        if Dice2 > 7:
+            print("\nYou've Paralyzed The Boss! You Get Another Turn.")
+            
+            while True:
+                In2 = input("Attack - 1 Heal - 2: ")
+                if In2 == "1" or In2 == "2":
+                    break
+                else:
+                    print("Invalid Command")
+                    
+            if In2 == "1":
+                Boss = Boss - Dice3
+                if SecretDice == 24:
+                    Boss = Boss - 1000
+                    print(Fore.RED + "\nBLACK FLASH! 1 out of 1000 Miracle! The Boss HP Is Now " + str(Boss) + Fore.WHITE + "\n")
+                    print("You Won!")
+                    exit()
+                    
+                print("\nYou Damaged The Boss By " + Fore.BLUE + str(Dice3) + Fore.WHITE + " The Boss Health Is Now " + Fore.RED +str(Boss) + Fore.WHITE +"\n")
+                if Dice3 > 20:
+                    print(Fore.GREEN + "A Critical Hit!" + Fore.WHITE)
+                if Boss < 0 or Boss == 0:
+                    print("You Won!")
+                    exit()
+                    
+            if In2 == "2":
+                Player = Player + 20
+                print("You Healed, Your Health Is Now " + Fore.GREEN + str(Player) + Fore.WHITE)
+                
+        if Boss < 0 or Boss == 0:
+            print("\nYou Won!")
+            break
+            
+    if In == "2":
+        Player = Player + 20
+        print("You Healed, Your Health Is Now " + Fore.GREEN + str(Player) + Fore.WHITE)
+    
+    for Ba in range(3, 0, -1):
+        print("\n" + str(Ba) + "...\n")
+        time.sleep(0.8)
+        BossDice1 = random.randint(1, 30)
+        BossDice2 = random.randint(1, 2)
+            
+        if Ba == 1:
+            if BossDice2 == 1:
+                Player = Player - BossDice1
+                print("The Boss Damaged You By " + Fore.BLUE + str(BossDice1) + Fore.WHITE + " Your Health Is Now " + Fore.GREEN + str(Player) +Fore.WHITE)
+                if BossDice1 > 20:
+                    print(Fore.RED + "A Critical Hit!" + Fore.WHITE)
+            if BossDice2 == 2:
+                Boss = Boss + 20
+                print("The Boss Healed, His Health Is Now " + Fore.RED + str(Boss) + Fore.WHITE)
+                 
+        if Player < 0 or Player == 0:
+            print("You Died.")
+            exit()
